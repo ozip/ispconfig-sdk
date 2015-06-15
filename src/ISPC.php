913e0e6b7503332077cbc049cec45cf512d326fe
martin adiyono <martin@martins-MacBook-Pro.local>
2015-06-15 12:35:12 +0700

    first commit

---
+++ src/ISPC.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: martin
+ * Date: 6/15/15
+ * Time: 12:08 PM
+ */
+
+namespace Ozip\ISPConfigSDK;
+
+
+class ISPC {
+
+    public function about(){
+        return "OZIP ISPConfig SDK";
+    }
+
+}
